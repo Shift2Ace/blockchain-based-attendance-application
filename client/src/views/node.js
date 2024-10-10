6ea4dc7fd4336c522d5fbfd2d5f656defa92b5a7
Shift2Ace <hei20030719@gmail.com>
2024-10-10 22:28:34 +0800

    localhost limit in Node page

--- client/src/views/node.js
+++ client/src/views/node.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { Navigate } from 'react-router-dom';
 import MenuBar from './components/menu';
 
+const NodePage = () => {
+  // Check if the hostname is localhost
+  const isLocalhost = window.location.hostname === 'localhost';
 
-const node = () => (
-  <div>
-    <MenuBar />
-    <h1>Node</h1>
-  </div>
-);
+  if (!isLocalhost) {
+    // Redirect to home page if not localhost
+    return <Navigate to="/home" />;
+  }
 
-export default node;
+  return (
+    <div>
+      <MenuBar />
+      <h1>Node Page</h1>
+      <p>This page can only be accessed from localhost.</p>
+    </div>
+  );
+};
+
+export default NodePage;
