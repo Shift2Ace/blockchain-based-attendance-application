53d70658257c3b89c4d4133c9d59256402567338
Shift2Ace <hei20030719@gmail.com>
2024-10-11 13:15:18 +0800

    add auto update

--- client/src/views/home.js
+++ client/src/views/home.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import MenuBar from './components/menu';
 import config from './components/config.json';
 
-
 const HomePage = () => {
   const [data, setData] = useState(null);
 
@@ -18,6 +17,9 @@ const HomePage = () => {
     };
 
     fetchData();
+    const intervalId = setInterval(fetchData, 5000); // Poll every 5 seconds
+
+    return () => clearInterval(intervalId); // Cleanup interval on component unmount
   }, []);
 
   return (
